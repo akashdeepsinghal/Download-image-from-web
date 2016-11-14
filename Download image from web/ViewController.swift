6401ea169285b9ae2581366a171b2cf100a2ac01
akigupta131 <akigupta131@gmail.com>
2016-11-14 19:22:44 +0530

    Downloading image from web and displaying successfully

--- Download image from web/ViewController.swift
+++ Download image from web/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var bachImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2@2x.png")!
+        
+        let request = NSMutableURLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print(error ?? "Some Error is there!")
+            } else {
+                if let data = data {
+                    if let bachImage = UIImage(data: data) {
+                        self.bachImageView.image = bachImage
+                    }
+                }
+            }
+        }
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
